b795931907438680d643397b01ae4fcd4cd6e5f4
zaxwebs <zifidigital@gmail.com>
2023-08-20 10:16:52 +0530

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/components/TodoList.svelte -->
+<script>
+	import { onMount } from 'svelte'
+	import { db } from '$lib/firebase'
+	import { addDoc, getDocs, collection, doc, updateDoc } from 'firebase/firestore'
+
+	let todos = []
+	let newTodo = ''
+
+	async function fetchTodos() {
+		const querySnapshot = await getDocs(collection(db, 'todos'))
+		todos = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+	}
+
+	async function addTodo() {
+		if (newTodo.trim() !== '') {
+			try {
+				const docRef = await addDoc(collection(db, 'todos'), {
+					text: newTodo,
+					completed: false,
+				})
+				newTodo = ''
+				await fetchTodos()
+				console.log('Document written with ID: ', docRef.id)
+			} catch (error) {
+				console.error('Error adding document: ', error)
+			}
+		}
+	}
+
+	async function toggleCompleted(todoId, isComplete) {
+		const todoRef = doc(db, 'todos', todoId)
+
+		try {
+			await updateDoc(todoRef, {
+				completed: !isComplete,
+			})
+			console.log(`Updated todo status`)
+		} catch (error) {
+			console.error('Error updating todo:', error)
+		}
+		fetchTodos()
+	}
+
+	onMount(fetchTodos)
+</script>
+
+<section>
+	<input bind:value={newTodo} placeholder="Enter a new todo" />
+	<button on:click={addTodo}>Add Todo</button>
+	<ul>
+		{#each todos as todo (todo.id)}
+			<li>
+				{todo.text}
+				<button on:click={() => toggleCompleted(todo.id, todo.completed)}>
+					{#if todo.completed}
+						Done
+					{:else}
+						Pending
+					{/if}
+				</button>
+			</li>
+		{/each}
+	</ul>
+</section>
